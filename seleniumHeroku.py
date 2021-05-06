f8af24b5a3b9677d3f84dc83ed03149b38cf2bd2
Hemal Gokhale <31528745+gokhalehemal11@users.noreply.github.com>
2021-05-06 23:07:30 +0530

    Update seleniumHeroku.py

--- seleniumHeroku.py
+++ seleniumHeroku.py
@@ -3,7 +3,6 @@ import os
 
 options = webdriver.ChromeOptions()
 options.binary_location= os.environ.get("GOOGLE_CHROME_BIN")
-options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
@@ -14,7 +13,8 @@ options.add_argument("Host= cdn-api.co-vin.in")
 options.add_argument("sec-ch-ua= ' Not A;Brand';v='99', 'Chromium';v='90', 'Google Chrome';v='90'")
 options.add_argument("Connection= keep-alive")
 options.add_argument("Accept= text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-options.add_argument("Access-Control-Allow-Origin= *")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
 
 driver= webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), options= options)
 driver.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=146&date=06-05-2021")
