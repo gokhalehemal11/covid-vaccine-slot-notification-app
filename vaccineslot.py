5a97eafe99a600d64c6c1e2a3337673ad7dc7f8a
Hemal Gokhale <31528745+gokhalehemal11@users.noreply.github.com>
2021-05-06 14:17:36 +0530

    Update vaccineslot.py

--- vaccineslot.py
+++ vaccineslot.py
@@ -2,6 +2,7 @@
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+import requests
 
 # define the scope
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
@@ -23,3 +24,21 @@ records_data = sheet_instance.get_all_records()
 
 # view the data
 print(records_data)
+
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Mobile Safari/537.36',
+		'Connection': 'keep-alive',
+    'Referer': 'www.cowin.gov.in',
+    'accept': 'application/json',
+		'accept-Language': 'en-US'
+		}
+
+PARAMS= {'pincode':'413608', 'date': '06-05-2021'}
+URL= "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=413608&date=06-05-2021"
+
+
+session = requests.Session()
+r = session.get(URL, headers=HEADERS, timeout=5)
+print(r.status_code)
+print(r.json())
