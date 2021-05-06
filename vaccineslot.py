ca589775764aaf3bd2fd930fe9fc6ec42c8d2449
Hemal Gokhale <31528745+gokhalehemal11@users.noreply.github.com>
2021-05-06 09:03:15 +0530

    Update vaccineslot.py

--- vaccineslot.py
+++ vaccineslot.py
@@ -1,42 +1,23 @@
 from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+
 from google.oauth2 import service_account
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
-import gspread
 
 # If modifying these scopes, delete the file token.json.
 SERVICE_ACCOUNT_FILE= "keys.json"
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://spreadsheets.google.com/feeds']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-#credentials= None
-#credentials= service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes= SCOPES)
+credentials= service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes= SCOPES)
 
 # The ID and range of a sample spreadsheet.
-#SPREADSHEET_ID = '1AhtW8GNzWqyQMzLNKgZLpGGsZaDT4XEJmMH1PQgGbgw'
+SPREADSHEET_ID = '1AhtW8GNzWqyQMzLNKgZLpGGsZaDT4XEJmMH1PQgGbgw'
 
-#service = build('sheets', 'v4', credentials=credentials)
+service = build('sheets', 'v4', credentials=credentials)
 
 # Call the Sheets API
-#sheet = service.spreadsheets()
-#result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-#                            range="responses!A1:G4").execute()
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                            range="responses!A1:G4").execute()
 
 #values = result.get('values', [])
-# add credentials to the account
-creds = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT_FILE, SCOPES)
-
-# authorize the clientsheet 
-client = gspread.authorize(creds)
-
-# get the instance of the Spreadsheet
-sheet = client.open('VaccineSlotApp')
-
-# get the first sheet of the Spreadsheet
-sheet_instance = sheet.get_worksheet(0)
-
-# get all the records of the data
-records_data = sheet_instance.get_all_records()
-
-# view the data
-
-print(records_data)
+print(result)
